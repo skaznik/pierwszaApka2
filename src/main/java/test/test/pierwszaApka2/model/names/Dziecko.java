dc4616d2fb04351d48d513e8e3b01d3c15f7c73f
skaznik <skaznik@wszib.edu.pl>
2020-01-25 14:35:12 +0100

    Spring-pierwsza apka

--- src/main/java/test/test/pierwszaApka2/model/names/Dziecko.java
+++ src/main/java/test/test/pierwszaApka2/model/names/Dziecko.java
@@ -3,7 +3,6 @@ package test.test.pierwszaApka2.model.names;
 public class Dziecko {
 
     public void daj(Prezent prezent) {
-        prezent.getNazwa();
-        System.out.println("Masz prezent");
+        System.out.println("Masz prezent : " + prezent.getNazwa());
     }
 }
--- src/main/java/test/test/pierwszaApka2/model/names/Mikolaj.java
+++ src/main/java/test/test/pierwszaApka2/model/names/Mikolaj.java
@@ -1,5 +1,6 @@
 package test.test.pierwszaApka2.model.names;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +10,24 @@ public class Mikolaj {
     private Dziecko malgosia;
     private Dziecko zbus;
 
+
+
     public Mikolaj(@Qualifier("zlosliwiec") Dziecko jas, @Qualifier("dziewczynka") Dziecko malgosia, @Qualifier("lobuz") Dziecko zbus) {
         this.jas = jas;
         this.malgosia = malgosia;
         this.zbus = zbus;
+    }
+    @Autowired
+    public void setDlaJasia(Prezent dlaJasia) {
+        this.jas.daj(dlaJasia);
+    }
+    @Autowired
+    public void setDalMalgosi(Prezent dlaMalgosi) {
+        this.malgosia.daj(dlaMalgosi);
+    }
+    @Autowired
+    public void setDlaZbusia(Prezent dlaZbusia) {
+        this.zbus.daj(dlaZbusia);
     }
 
 }
