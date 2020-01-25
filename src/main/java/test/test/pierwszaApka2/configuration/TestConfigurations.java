a866bf55457b164f1414f839f4b88e2b7f7d25b6
skaznik <skaznik@wszib.edu.pl>
2020-01-25 11:12:00 +0100

    Spring-pierwsza apka

--- src/main/java/test/test/pierwszaApka2/configuration/TestConfigurations.java
+++ src/main/java/test/test/pierwszaApka2/configuration/TestConfigurations.java
@@ -2,13 +2,30 @@ package test.test.pierwszaApka2.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import test.test.pierwszaApka2.model.Kierowca;
+import test.test.pierwszaApka2.model.PrawoJazdy;
 import test.test.pierwszaApka2.model.Samochod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class TestConfigurations {
 
     @Bean
-     public Samochod samochod() {
-         return new Samochod();
+     public List<Samochod> samochod() {
+         List<Samochod> out = new ArrayList<>();
+         for(int i = 0; i<5;++i) {
+             out.add(new Samochod());
+         }
+         return out;
+     }
+     @Bean
+    public PrawoJazdy prawoJazdy(){
+        return new PrawoJazdy();
+     }
+     @Bean
+    public Kierowca kierowca() {
+        return new Kierowca();
      }
 }
--- src/main/java/test/test/pierwszaApka2/model/Kierowca.java
+++ src/main/java/test/test/pierwszaApka2/model/Kierowca.java
@@ -2,6 +2,6 @@ package test.test.pierwszaApka2.model;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Kierowca {
 }
