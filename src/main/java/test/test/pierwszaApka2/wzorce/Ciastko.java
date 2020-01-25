4a26d53e6c91f5c2fe10e7ed23e2b4328394d898
skaznik <skaznik@wszib.edu.pl>
2020-01-25 16:09:53 +0100

    Spring-pierwsza apka

--- src/main/java/test/test/pierwszaApka2/wzorce/Ciastko.java
+++ src/main/java/test/test/pierwszaApka2/wzorce/Ciastko.java
@@ -1,10 +1,12 @@
 package test.test.pierwszaApka2.wzorce;
 
-import org.springframework.beans.factory.config.BeanDefinition;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Scope(BeanDefinition.SCOPE_PROTOTYPE) //domyślne jest singelton, aby go zmienić - trzeba dodać tą linijkę
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+@Scope(SCOPE_PROTOTYPE) //domyślne jest singelton, aby go zmienić - trzeba dodać tą linijkę
 @Component
 public class Ciastko {
 
