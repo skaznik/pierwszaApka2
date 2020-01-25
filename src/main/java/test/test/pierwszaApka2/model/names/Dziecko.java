33c407b4901c592c249e3e53ce71da3a554f5a2b
skaznik <skaznik@wszib.edu.pl>
2020-01-25 14:16:45 +0100

    Spring-pierwsza apka

--- src/main/java/test/test/pierwszaApka2/model/names/Dziecko.java
+++ src/main/java/test/test/pierwszaApka2/model/names/Dziecko.java
@@ -3,7 +3,9 @@ package test.test.pierwszaApka2.model.names;
 public class Dziecko {
 
     public void daj(Prezent prezent) {
-
+        prezent.getNazwa();
+        prezent.setNazwa();
+        prezent.getNazwa2();
         System.out.println("Masz prezent");
     }
 }
--- src/main/java/test/test/pierwszaApka2/model/names/Prezent.java
+++ src/main/java/test/test/pierwszaApka2/model/names/Prezent.java
@@ -5,4 +5,5 @@ import lombok.Data;
 @Data
 public class Prezent {
     private String nazwa;
+    private String nazwa2;
 }
